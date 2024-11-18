3732947dbd485bcaeb45f4d0623df4316c2944a9
Edel Nurintan Saragih <edelnurintan@gmail.com>
2024-11-18 09:38:42 +0700

    Add files via upload

---
+++ Pemasok.sql
@@ -0,0 +1,59 @@
+USE INFORMASI;
+
+INSERT INTO Pemasok (Id_Pemasok, Nama_Pemasok, Alamat, Kontak)
+VALUES
+(1, 'PT. Logam Makmur', 'Jl. Industri No.1, Jakarta', '021-12345678'),
+(2, 'CV. Kencana Plastik', 'Jl. Raya Kencana No.5, Bandung', '022-87654321'),
+(3, 'UD. Baja Pratama', 'Jl. Baja Utama No.10, Surabaya', '031-5551234'),
+(4, 'PT. Tekstil Nusantara', 'Jl. Sutra No.11, Solo', '0271-2345678'),
+(5, 'PT. Elektronik Global', 'Jl. Elektrik No.21, Bekasi', '021-99887766'),
+(6, 'CV. Fiber Optik Jaya', 'Jl. Telekomunikasi No.45, Tangerang', '021-44556677'),
+(7, 'PT. Kimia Persada', 'Jl. Kimia No.18, Medan', '061-33445566'),
+(8, 'UD. Serat Alam', 'Jl. Serat No.22, Yogyakarta', '0274-99887766'),
+(9, 'PT. Komposit Mandiri', 'Jl. Resin No.9, Semarang', '024-55667788'),
+(10, 'CV. Kaca Mulia', 'Jl. Cermin No.7, Malang', '0341-22334455'),
+(11, 'PT. Baja Ringan', 'Jl. Baja No.13, Palembang', '0711-66778899'),
+(12, 'CV. Tembaga Prima', 'Jl. Logam Mulia No.4, Pekanbaru', '0761-22334455'),
+(13, 'PT. Plastik Utama', 'Jl. Plastik No.14, Cirebon', '0231-44556677'),
+(14, 'UD. Kayu Jati', 'Jl. Kayu No.15, Balikpapan', '0542-55667788'),
+(15, 'CV. Serat Karbon', 'Jl. Komposit No.19, Pontianak', '0561-77889900'),
+(16, 'PT. Logistik Mineral', 'Jl. Mineral No.20, Makassar', '0411-88990011'),
+(17, 'UD. Kain Batik', 'Jl. Sutra No.25, Denpasar', '0361-33445566'),
+(18, 'PT. Polimer Terbaik', 'Jl. Polimer No.30, Manado', '0431-22334455'),
+(19, 'CV. Cat Warna Jaya', 'Jl. Cat No.32, Mataram', '0370-44556677'),
+(20, 'UD. Keramik Nusantara', 'Jl. Keramik No.33, Palu', '0451-55667788'),
+(21, 'PT. Karet Alam', 'Jl. Karet No.34, Samarinda', '0541-77889900'),
+(22, 'CV. Logam Perkasa', 'Jl. Logam Perkasa No.35, Banjarmasin', '0511-88990011'),
+(23, 'PT. Resin Indonesia', 'Jl. Resin No.36, Serang', '0254-22334455'),
+(24, 'UD. Kulit Asli', 'Jl. Kulit No.37, Banda Aceh', '0651-33445566'),
+(25, 'PT. Baja Super', 'Jl. Baja Super No.38, Bandar Lampung', '0721-44556677'),
+(26, 'CV. Besi Jaya', 'Jl. Besi No.39, Tasikmalaya', '0265-55667788'),
+(27, 'UD. Serat Optik', 'Jl. Telekomunikasi No.40, Ambon', '0911-66778899'),
+(28, 'PT. Aluminium Asia', 'Jl. Aluminium No.41, Batam', '0778-88990011'),
+(29, 'CV. Komposit Global', 'Jl. Komposit No.42, Padang', '0751-22334455'),
+(30, 'PT. Plastik Recycle', 'Jl. Daur Ulang No.43, Kupang', '0380-33445566');
+
+INSERT INTO Pemasok (Id_Pemasok, Nama_Pemasok, Alamat, Kontak)  
+VALUES  
+(31, 'UD. Kayu Alam', 'Jl. Hutan No.44, Palangka Raya', '0536-77889900'),  
+(32, 'PT. Serat Plastik', 'Jl. Polimer No.45, Gorontalo', '0435-88990011'),  
+(33, 'CV. Logam Indah', 'Jl. Besi No.46, Ternate', '0921-22334455'),  
+(34, 'UD. Tekstil Modern', 'Jl. Pakaian No.47, Jayapura', '0967-33445566'),  
+(35, 'PT. Komposit Utama', 'Jl. Fiber No.48, Kendari', '0401-44556677'),  
+(36, 'CV. Keramik Antik', 'Jl. Seni No.49, Jambi', '0741-55667788'),  
+(37, 'UD. Plastik Surya', 'Jl. Plastik No.50, Pangkal Pinang', '0717-77889900'),  
+(38, 'PT. Baja Tangguh', 'Jl. Baja No.51, Maluku Utara', '0922-88990011'),  
+(39, 'CV. Serat Nusantara', 'Jl. Sutra No.52, Sulawesi Tenggara', '0402-22334455'),  
+(40, 'UD. Kain Tenun', 'Jl. Tenun No.53, NTT', '0381-33445566'),  
+(41, 'PT. Polimer Mandiri', 'Jl. Kimia No.54, Banten', '0251-44556677'),  
+(42, 'CV. Cat Cerah', 'Jl. Warna No.55, Sulawesi Tengah', '0452-55667788'),  
+(43, 'UD. Kulit Sejati', 'Jl. Kulit No.56, NTB', '0371-77889900'),  
+(44, 'PT. Aluminium Prima', 'Jl. Logam No.57, Batam', '0778-88990022'),  
+(45, 'CV. Fiber Optik Nusantara', 'Jl. Fiber No.58, Balikpapan', '0542-66778899'),  
+(46, 'UD. Keramik Maju', 'Jl. Keramik No.59, Kalimantan Tengah', '0536-88990033'),  
+(47, 'PT. Baja Perkasa', 'Jl. Baja No.60, Sulawesi Selatan', '0411-22334455'),  
+(48, 'CV. Besi Berkualitas', 'Jl. Besi No.61, Aceh', '0651-44556677'),  
+(49, 'UD. Serat Optik Dunia', 'Jl. Telekomunikasi No.62, Maluku', '0911-55667788'),  
+(50, 'PT. Plastik Hijau', 'Jl. Recycle No.63, Papua', '0967-77889900');  
+
+SELECT*FROM Pemasok
